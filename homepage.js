82713f8305ff606f0af201bbe7e9f8ee6ce4a1b6
Lance Woloszyn <ljwolos@pointpark.edu>
2018-09-17 15:55:41 -0400

    first commit homepage all beggining stuff

---
+++ homepage.js
@@ -0,0 +1,40 @@
+var express = require('express');
+
+var app = express();
+
+// set up handlebars
+var handlebars = require('express-handlebars');
+	.create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res){
+	res.type('text/plain');
+	res.send('Homepage');
+});
+app.get('/about', function(req, res){
+	res.type('text/plain');
+	res.send('About Homepage');
+});
+
+// 404 page
+app.use(function(req, res){
+	res.type('text/plain');
+	res.status(404)
+	res.send('404 - Not Found');
+});
+
+// 500 page
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+ console.log( 'Express started on host:' + app.get('port') + '; press ctrl-c to terminate.');
+});
+	
