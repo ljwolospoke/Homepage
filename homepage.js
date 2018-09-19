3e701cafc9d3768fe9f14064df28ceaf0393bab9
Lance Woloszyn <ljwolos@pointpark.edu>
2018-09-19 16:22:00 -0400

    finalized simplfied version

--- homepage.js
+++ homepage.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 // set up handlebars
-var handlebars = require('express-handlebars');
+var handlebars = require('express-handlebars')
 	.create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -11,27 +11,28 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res){
-	res.type('text/plain');
-	res.send('Homepage');
+	res.render('home');
 });
+
+app.get('/game', function(req, res){
+	res.render('game');
+});
+
 app.get('/about', function(req, res){
-	res.type('text/plain');
-	res.send('About Homepage');
+	res.render('about');
 });
 
 // 404 page
-app.use(function(req, res){
-	res.type('text/plain');
+app.use(function(req, res, next){
 	res.status(404)
-	res.send('404 - Not Found');
+	res.render('404')
 });
 
 // 500 page
 app.use(function(err, req, res, next){
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
 });
 
 app.listen(app.get('port'), function(){
